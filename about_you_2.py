105d2ec2d2bccaacb8213e8328237dbc6ed6d62d
Gav McClary <gavin.mcclary@gmail.com>
2018-08-01 16:33:10 +0100

    added about_you_2.py and concat.py

---
+++ about_you_2.py
@@ -0,0 +1,10 @@
+# Using concatenation print the following on 1 line:
+
+'''
+1.Your name
+2.Your age
+3.Your favourite movie
+'''
+
+
+
---
+++ concat.py
@@ -0,0 +1,11 @@
+# An example of concatenation
+
+'''
+Concatenation: the action of linking things together in a series,
+or the condition of being linked in such a way.
+'''
+
+print("These" + "words" + "have" + "been" + "linked" + "together" + "using" +
+        "concatenation" + "but" + "how?" )
+
+        
