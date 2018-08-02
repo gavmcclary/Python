f875bdf6a33498b1eca75583d6661ac729db530e
Gav McClary <gavin.mcclary@gmail.com>
2018-08-02 12:51:42 +0100

    Changed else_condition.py

---
+++ else_condition.py
@@ -0,0 +1,6 @@
+
+if 1 == 1:
+    print("Statement 1")
+
+else:
+    print("Statement 3")
--- if_condition.py
+++ if_condition.py
@@ -3,7 +3,6 @@
 if 1 == 1:
     print("Of course 1 is equal to 1!")
 
-if 1 == 2:
-    print("Of course 1 is not equal to 2!")
+
 
     
